d879c1ae47aafd4992625a8d29dc2089c3652023
Mohamed Hassan <mohamedhassanwork2887@gmail.com>
2024-09-26 22:22:20 +0300

    Smooth Scrollbar

--- js/app.js
+++ js/app.js
@@ -53,8 +53,8 @@
 
 // Set sections as active
 
-// Interactive navbar
-// (Target the sections and the navbar, then create nav bar items, then add content and a class to these created items, then append them to navbar)
+// Interactive navbar with smooth scrolling
+// (Target the sections and the navbar, then create nav bar items, then add content and a class to these created items as well as an event listener to the link for smooth scrolling, then append them to navbar)
 const sections = document.querySelectorAll("section");
 
 const navList = document.getElementById("navbar__list");
@@ -66,6 +66,10 @@ sections.forEach((section) => {
   link.textContent = section.getAttribute("data-nav");
   link.href = `#${section.id}`;
   link.classList.add("menu__link");
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    section.scrollIntoView({ behavior: "smooth" });
+  });
 
   navItem.appendChild(link);
   navList.appendChild(navItem);
@@ -90,15 +94,12 @@ function checkVisibility() {
   elementsToCheck.forEach((element) => {
     if (isElementInViewport(element)) {
       console.log(`Element ${element.id} is visible in viewport`);
-      // Optionally, add an active class for styling
       element.classList.add("active");
     } else {
       console.log(`Element ${element.id} is not visible in viewport`);
-      // Optionally, remove the active class if not visible
       element.classList.remove("active");
     }
   });
 }
 
-// Attach the debounced scroll event listener
 window.addEventListener("scroll", checkVisibility);
