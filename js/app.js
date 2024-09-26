cea715562d0390c9131dd33a573aabe66266f5c0
Mohamed Hassan <mohamedhassanwork2887@gmail.com>
2024-09-27 00:33:22 +0300

    Scroll to top button

--- js/app.js
+++ js/app.js
@@ -122,3 +122,25 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("hidden");
   }, 2000);
 });
+
+// Scroll back button
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
